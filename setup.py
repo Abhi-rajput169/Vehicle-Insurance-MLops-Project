6d5738e08d75003ac4860e3a5e85eee9dd6409e8
Abhishek Rajput <rajputabhishek0108@gmail.com>
2025-08-12 19:35:41 +0530

    project folders done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='src',
+    version="0.0.1",
+    author="Abhishek Rajput",
+    author_email="rajputabhishek0108@gmail.com",
+    packages=find_packages()
+
+)
